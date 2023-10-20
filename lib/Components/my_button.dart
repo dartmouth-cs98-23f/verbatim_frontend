ad6b8e1a3b095628fa35eda24cde57acd38b4aae
DahliaTheCoder <dahlia.igiraneza.24@dartmouth.edu>
2023-10-20 18:11:54 -0400

    Updated sign up page--It can now make basic checks on the user's information when they sign up and the information is being saved in the verbatim database.

--- lib/Components/my_button.dart
+++ lib/Components/my_button.dart
@@ -15,50 +15,52 @@ class MyButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        width: 328,
-        height: 49,
-        padding: const EdgeInsets.symmetric(horizontal: 18),
-        decoration: ShapeDecoration(
-          color: Color(0xFF1E4693), // Set the background color here
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
-          ),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: 35,
-              height: 35,
-              decoration: BoxDecoration(
-                image: hasButtonImage
-                    ? DecorationImage(
-                  image: AssetImage('lib/images/googleImage.png'),
-                  fit: BoxFit.fill,
-                )
-                    : null,
-                color: Color(0xFF1E4693), // Set the same background color
-              ),
+    return SafeArea(
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          width: 333,
+          height: 49,
+          padding: const EdgeInsets.symmetric(horizontal: 18),
+          decoration: ShapeDecoration(
+            color: Color(0xFF1E4693), // Set the background color here
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
             ),
-            SizedBox(width: 10), // Add some spacing between the image and text
-            Align(
-              alignment: Alignment.center,
-              child: Text(
-                buttonText,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 14,
-                  fontFamily: 'Mulish',
-                  fontWeight: FontWeight.w700,
-                  height: 1.0, // Adjust height to center text vertically
-                  letterSpacing: 0.30,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 35,
+                height: 35,
+                decoration: BoxDecoration(
+                  image: hasButtonImage
+                      ? DecorationImage(
+                    image: AssetImage('lib/images/googleImage.png'),
+                    fit: BoxFit.fill,
+                  )
+                      : null,
+                  color: Color(0xFF1E4693), // Set the same background color
                 ),
               ),
-            ),
-          ],
+              SizedBox(width: 10), // Add some spacing between the image and text
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  buttonText,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontFamily: 'Mulish',
+                    fontWeight: FontWeight.w700,
+                    height: 1.0, // Adjust height to center text vertically
+                    letterSpacing: 0.30,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
