ecf27a999cda154c5361ff1f13c7b152164b990b
DahliaTheCoder <dahlia.igiraneza.24@dartmouth.edu>
2023-10-21 20:17:32 -0400

    Created the login page (Note: Backend communication not tested due to the absence of the respective endpoint).

--- lib/Components/my_button.dart
+++ lib/Components/my_button.dart
@@ -1,4 +1,3 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
@@ -23,43 +22,49 @@ class MyButton extends StatelessWidget {
           height: 49,
           padding: const EdgeInsets.symmetric(horizontal: 18),
           decoration: ShapeDecoration(
-            color: Color(0xFF1E4693), // Set the background color here
+            color: Color(0xFF1E4693),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(20),
             ),
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 35,
-                height: 35,
-                decoration: BoxDecoration(
-                  image: hasButtonImage
-                      ? DecorationImage(
-                    image: AssetImage('lib/images/googleImage.png'),
-                    fit: BoxFit.fill,
-                  )
-                      : null,
-                  color: Color(0xFF1E4693), // Set the same background color
-                ),
-              ),
-              SizedBox(width: 10), // Add some spacing between the image and text
-              Align(
-                alignment: Alignment.center,
-                child: Text(
-                  buttonText,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 14,
-                    fontFamily: 'Mulish',
-                    fontWeight: FontWeight.w700,
-                    height: 1.0, // Adjust height to center text vertically
-                    letterSpacing: 0.30,
+          child: Material( // Use Material widget to enable ink splash
+            color: Colors.transparent, // Make it transparent to prevent background color overlay
+            child: InkWell(
+              onTap: onTap,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 35,
+                    height: 35,
+                    decoration: BoxDecoration(
+                      image: hasButtonImage
+                          ? DecorationImage(
+                        image: AssetImage('lib/images/googleImage.png'),
+                        fit: BoxFit.fill,
+                      )
+                          : null,
+                      color: Color(0xFF1E4693),
+                    ),
                   ),
-                ),
+                  SizedBox(width: 10),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      buttonText,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontFamily: 'Mulish',
+                        fontWeight: FontWeight.w700,
+                        height: 1.0,
+                        letterSpacing: 0.30,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
