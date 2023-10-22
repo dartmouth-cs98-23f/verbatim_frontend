2bed65438ca9fd94bc29b02cb213eb5f80580916
DahliaTheCoder <dahlia.igiraneza.24@dartmouth.edu>
2023-10-21 20:17:32 -0400

    Created the login page (Note: Backend communication not tested due to the absence of the respective endpoint).

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:verbatim_frontend/screens/logIn.dart';
 import 'package:verbatim_frontend/screens/signUp.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SignUp(),
+      home: LogIn(),
     );
   }
 }
