87540d2e8e2209a892c4544f118fb10b1dd5304e
Eve Wening <81572401+evexwening@users.noreply.github.com>
2023-11-07 15:55:17 -0500

    friends stats!

--- lib/screens/globalChallenge.dart
+++ lib/screens/globalChallenge.dart
@@ -43,6 +43,7 @@ class _GlobalChallengeState extends State<globalChallenge> {
   int numVerbatimQ2 = 0;
   int numVerbatimQ3 = 0;
   int numExactVerbatim = 0;
+
   Map<String, dynamic> statsQ1 = {
     "firstMostPopular": "",
     "numResponsesFirst": 0,
@@ -50,6 +51,7 @@ class _GlobalChallengeState extends State<globalChallenge> {
     "numResponsesSecond": 0,
     "thirdMostPopular": "",
     "numResponsesThird": 0,
+    "friendResponses": [],
   };
   Map<String, dynamic> statsQ2 = {
     "firstMostPopular": "",
@@ -58,6 +60,7 @@ class _GlobalChallengeState extends State<globalChallenge> {
     "numResponsesSecond": 0,
     "thirdMostPopular": "",
     "numResponsesThird": 0,
+    "friendResponses": [],
   };
   Map<String, dynamic> statsQ3 = {
     "firstMostPopular": "",
@@ -66,6 +69,7 @@ class _GlobalChallengeState extends State<globalChallenge> {
     "numResponsesSecond": 0,
     "thirdMostPopular": "",
     "numResponsesThird": 0,
+    "friendResponses": [],
   };
 
   bool response = false;
@@ -145,7 +149,10 @@ class _GlobalChallengeState extends State<globalChallenge> {
 
     if (response.statusCode == 200) {
       print('Responses sent successfully');
+
       final Map<String, dynamic> stats = json.decode(response.body);
+      print(stats);
+
       numVerbatimQ1 = stats['numVerbatimQ1'];
       numVerbatimQ2 = stats['numVerbatimQ2'];
       numVerbatimQ3 = stats['numVerbatimQ3'];
@@ -153,6 +160,7 @@ class _GlobalChallengeState extends State<globalChallenge> {
       statsQ2 = stats['statsQ2'];
       statsQ3 = stats['statsQ3'];
       totalResponses = stats['totalResponses'];
+      print(statsQ1["friendResponses"]);
     } else {
       print('Failed to send responses. Status code: ${response.statusCode}');
 
