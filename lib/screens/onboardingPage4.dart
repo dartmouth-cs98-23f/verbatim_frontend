2d945edc8e3da9a07767e0c95ab370078d4c2128
DahliaTheCoder <dahlia.igiraneza.24@dartmouth.edu>
2023-11-12 11:25:42 -0500

    Made some minor changes on signup/in and onboarding pages to improve page navigation

--- lib/screens/onboardingPage4.dart
+++ lib/screens/onboardingPage4.dart
@@ -78,13 +78,8 @@ class OnBoardingPage4 extends StatelessWidget {
                 children: [
                   ElevatedButton(
                     onPressed: () {
-                      // Navigate to the 'Sign Up' page
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => globalChallenge(),
-                        ),
-                      );
+                      // Navigate to the 'Global Challenge' page
+                      Navigator.pushNamed(context, '/global_challenge');
                     },
                     style: ElevatedButton.styleFrom(
                       primary: Color(0xFFFFF3EE), // Background color
@@ -102,10 +97,8 @@ class OnBoardingPage4 extends StatelessWidget {
 
                   ElevatedButton(
                     onPressed: () {
-                      // Navigate to the 'Sign Up' page
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => globalChallenge(),
-                      ));
+                      // Navigate to the 'Global Challenge' page
+                      Navigator.pushNamed(context, '/global_challenge');
                     },
                     style: ElevatedButton.styleFrom(
                       primary: Color(0xFFE76F51), // Background color
