ba05ef8fbb559faef32962c5873ebf23f2c5c69a
Eve Wening <81572401+evexwening@users.noreply.github.com>
2023-11-12 21:01:38 -0500

    no scroll in onboarding

--- lib/screens/onboardingPage4.dart
+++ lib/screens/onboardingPage4.dart
@@ -3,6 +3,8 @@ import 'package:verbatim_frontend/screens/getStarted.dart';
 import 'package:verbatim_frontend/screens/signUp.dart';
 import 'package:verbatim_frontend/Components/shared_prefs.dart';
 import 'globalChallenge.dart';
+import 'package:verbatim_frontend/widgets/size.dart';
+
 import 'logIn.dart';
 
 class OnBoardingPage4 extends StatelessWidget {
@@ -21,18 +23,18 @@ class OnBoardingPage4 extends StatelessWidget {
                   child: Column(
                     children: [
                       Padding(
-                        padding: const EdgeInsets.only(top: 60.0),  //Color(0xFFFFF3EE)
+                        padding: const EdgeInsets.only(
+                            top: 30.0), //Color(0xFFFFF3EE)
                         child: Align(
                           alignment: Alignment.center,
                           child: Image.asset(
                             'assets/onboardingPage4Image.png', // Replace with the path to your image asset
-                            width: 454, // Set the width and height to your preference
-                            height: 354,
+                            width: 454
+                                .v, // Set the width and height to your preference
+                            height: 354.v,
                           ),
                         ),
                       ),
-
-                      const SizedBox(height: 15),
                       Center(
                         child: Text(
                           'Custom \nChallenges',
@@ -47,8 +49,7 @@ class OnBoardingPage4 extends StatelessWidget {
                           ),
                         ),
                       ),
-
-                      const SizedBox(height: 40),
+                      SizedBox(height: 30.v),
                       Padding(
                         padding: const EdgeInsets.only(left: 30.0, right: 30.0),
                         child: Align(
@@ -72,7 +73,7 @@ class OnBoardingPage4 extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(40.0),
+              padding: const EdgeInsets.all(15.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -84,7 +85,8 @@ class OnBoardingPage4 extends StatelessWidget {
                     style: ElevatedButton.styleFrom(
                       primary: Color(0xFFFFF3EE), // Background color
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(13.0), // Rectangular shape
+                        borderRadius:
+                            BorderRadius.circular(13.0), // Rectangular shape
                       ),
                     ),
                     child: Text(
@@ -94,7 +96,6 @@ class OnBoardingPage4 extends StatelessWidget {
                       ),
                     ),
                   ),
-
                   ElevatedButton(
                     onPressed: () {
                       // Navigate to the 'Global Challenge' page
@@ -103,7 +104,8 @@ class OnBoardingPage4 extends StatelessWidget {
                     style: ElevatedButton.styleFrom(
                       primary: Color(0xFFE76F51), // Background color
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(13.0), // Rectangular shape
+                        borderRadius:
+                            BorderRadius.circular(13.0), // Rectangular shape
                       ),
                     ),
                     child: Text(
