0ad6720f599fa95b49745cee9a3662cc69138d43
DahliaTheCoder <dahlia.igiraneza.24@dartmouth.edu>
2024-02-09 11:11:50 -0500

    Merged main into my branch and things are working fine after this merge (at least I think they are).

--- lib/screens/logIn.dart
+++ lib/screens/logIn.dart
@@ -107,9 +107,8 @@ class _LogInState extends State<LogIn> {
       } else {
         print('Error during log-in: ${response.statusCode.toString()}');
         Navigator.of(context).push(MaterialPageRoute(
-            builder: (context) => const SignupErrorMessage(pageName: 'log in')));
-
-            builder: (context) => SignupErrorMessage(pageName: 'log in')));
+            builder: (context) =>
+                const SignupErrorMessage(pageName: 'log in')));
       }
     } catch (e) {
       print('Error during sign-up: $e');
