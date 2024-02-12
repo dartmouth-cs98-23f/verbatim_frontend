60944a4b38ae8bbb9510ad8038153fe158f758c6
Eve Wening <81572401+evexwening@users.noreply.github.com>
2024-02-11 20:26:43 -0500

    starts on landing page

--- lib/main.dart
+++ lib/main.dart
@@ -79,7 +79,7 @@ class MyApp extends StatelessWidget {
       navigatorKey: navigatorKey,
       onGenerateRoute: Application.router.generator,
       //    initialRoute: SharedPrefs().getCurrentPage() ?? '/landingPage',
-      home: const LogIn(),
+      home: const LandingPage(),
     );
   }
 }
